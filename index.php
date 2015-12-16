9fbf304a4a28d5954044a70d9529a0ca9b881015
Mohale Molieleng <mjmolieleng@gmail.com>
2015-12-16 18:40:57 +0200

    Wiwa Initial

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>wiinc wallet</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="font.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php  include("templates/header.php"); ?>
+
+    <div class="container-fluid header" style="text-align:center; margin-top:-20px;">
+      
+      <div class="row ">
+        <h1 class="center main-title">Start transfering money and paying for stuff online</h1>
+      </div>
+
+      <div class="row center getstarted">
+        <a href="" class="btn btn-primary btn-lg">Get Started</a>
+      </div>
+    </div>
+
+    <div class="container text-center">
+
+      <div class="row below">
+
+        <h1 class="center">Introducing online payments using MPESA and Eco-Cash</h1>
+        
+      </div>
+
+
+      <div class="row">
+
+
+        <!-- First Thumbnail -->
+        <div class="col-sm-6 col-md-4">
+
+          <div class="thumbnail text-center">
+          
+              <span class="glyphicon glyphicon-link icon"></span>
+
+              <div class="caption">
+
+                <h3 class="text-center">A link between different mobile money platforms</h3>
+
+                <p class="text-center">Transfer money with wiincpay to your friends and businesses on different networks.</p>
+
+              </div>
+
+          </div>
+        
+        </div>       
+        <!-- End of Thumbnail -->
+
+        <!-- First Thumbnail -->
+        <div class="col-sm-6 col-md-4">
+
+          <div class="thumbnail text-center">
+          
+             <span class="glyphicon glyphicon-phone icon"></span>
+
+              <div class="caption">
+
+                <h3 class="text-center">We focus on speed</h3>
+
+                <p class="text-center">Download the android app to make payments even faster.</p>
+
+              </div>
+
+          </div>
+        
+        </div>       
+        <!-- End of Thumbnail -->
+
+        <!-- First Thumbnail -->
+        <div class="col-sm-6 col-md-4">
+
+          <div class="thumbnail text-center">
+          
+              <span class="glyphicon glyphicon-stats icon" style=""></span>
+
+              <div class="caption">
+
+                <h3 class="text-center">Business and personal Analytics</h3>
+
+                <p class="text-center">Keep track of every payment via the wiinc-wallet statics and analytics tools </p>
+
+              </div>
+
+          </div>
+        
+        </div>       
+        <!-- End of Thumbnail -->
+      </div>
+
+    </div>
+
+    <div class="container-fluid signupBody">
+
+      <div class="row text-center">
+        <h1>Your money is in good, secure place</h1>
+        <a href="" class="btn btn-primary accbtn btn-lg">Create your account</a>
+      </div>
+    </div>
+
+
+    <!-- End of boday -->
+
+    <!-- Footer -->
+    <div class="container-fluid footer">
+
+      <div class="container">
+
+        <div class="row text-center">
+          <!--
+          <div class="col-md-6">
+
+            <h1 class="text-left">Contact Us</h1>
+            <address>
+              <p>Tel : 00266 59694888</p>
+
+            </address>
+            <p class="text-left">
+              
+            </p>
+          </div>
+
+        -->
+
+        <h1>Contact Us</h1>
+          <h3>00266 5969 4888</h3>
+          <p class="text-center">mohale@wiincwallet.com | tlotlisomafantiri@wiincwallet.com</p>
+        </div>
+
+        
+
+        <div class="row text-center morelinks">
+          <span class="lower-links"><a href="">Help</a></span>
+          <span class="lower-links"><a href="">Contact</a></span>
+          <span class="lower-links"><a href="">Fees</a></span>
+          <span class="lower-links"><a href="">Security</a></span>
+          <span class="lower-links"><a href="">About</a></span>
+          <span class="lower-links"><a href="">Partners</a></span>
+        </div>
+
+        <p class="text-center">
+          Users are advised to read the <a href="">terms and conditions</a> carefully
+        </p>
+
+      </div>
+
+      <div class="row min-footer text-center">
+        <p class="text-center label">Made by <a href="wiinc.co.ls">wiinc</a></p>
+      </div>
+    </div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
